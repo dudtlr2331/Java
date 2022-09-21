d8b6627efaa1ab0fa5d48f7700e587073b3ba228
Cat Hanbit <dudtlr102@naver.com>
2022-09-21 10:46:19 +0900

    CompLogicDemo

---
+++ src/week04/CompLogicDemo.java
@@ -0,0 +1,13 @@
+package week04;
+
+public class CompLogicDemo {
+
+	public static void main(String[] args) {
+		int x = 0, y = 1;
+		System.out.println((x < 1) || (y-- < 1));
+		System.out.println("x = " + x + ", y = " + y);
+		
+		System.out.println(y);
+	}
+
+}
