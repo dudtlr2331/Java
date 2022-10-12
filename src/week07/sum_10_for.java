acd940086816af96520b27b0d03c5417142d8f1e
Cat Hanbit <dudtlr102@naver.com>
2022-10-12 10:47:43 +0900

    1~10까지의 합 for문

---
+++ src/week07/sum_10_for.java
@@ -0,0 +1,12 @@
+package week07;
+
+public class sum_10_for {
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		for(int i=1; i <= 10; i++)
+			sum += i;
+		
+		System.out.printf("1~10ÀÇ ÇÕ°è : %d\n", sum);
+	}
+}
