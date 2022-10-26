246be9198539a1aa99aaeeec8d7b8a51e1190054
Cat Hanbit <dudtlr102@naver.com>
2022-10-26 23:39:46 +0900

    and, or

---
+++ src/week07/Ex5.java
@@ -0,0 +1,12 @@
+package week07;
+
+public class Ex5 {
+	public static void main(String[] args) {
+		
+		int a = 20;
+		int b = 300;
+		int c = 4000;
+		
+		System.out.printf("%s", (a>30||b>500)&&(c == 4000));
+	}
+}
