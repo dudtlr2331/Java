9334d0b87bafc927fba816e89e0cd63d9ed1fea6
Cat Hanbit <dudtlr102@naver.com>
2022-11-09 16:32:45 +0900

    예제3

---
+++ src/week11/Ex03.java
@@ -0,0 +1,14 @@
+package week11;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		int x = 0;
+		System.out.println("increment() 메소드 호출 전 x : " + x);
+		increment(x);
+		System.out.println("increment() 메소드 호출 후 x : " + x);
+	}
+	
+	public static void increment(int n) {
+		n++;
+	}
+}
