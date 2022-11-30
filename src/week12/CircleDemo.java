0b70fbbdc9c19dc0c4b9a14395cdf66d202caf5e
Cat Hanbit <dudtlr102@naver.com>
2022-11-30 09:50:52 +0900

    12주차 예제

---
+++ src/week12/CircleDemo.java
@@ -0,0 +1,28 @@
+package week12;
+
+public class CircleDemo { 
+	public static void main(String[] args) {
+		double area;
+		
+		Circle myCircle = new Circle();
+		
+		myCircle.radius = 10.0;
+		
+		area = myCircle.findArea();
+		
+		myCircle.show(myCircle.radius, area);
+	}
+}
+
+class Circle{
+	double radius;
+	
+	double findArea() {
+		return 3.14 * radius * radius;
+	}
+	
+	void show(double x, double y) {
+		double area = findArea();
+		System.out.printf("¹ÝÁö¸§ = %.1f, ³ÐÀÌ = %.1f\n", x, y);
+	}
+}
